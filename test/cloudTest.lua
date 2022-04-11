8a36b19734092e6a6afc48cbf16cb4b24ae26ddf
1 <1>
2022-04-11 20:17:38 +0800

    feat: add cloud func

--- test/cloudTest.lua
+++ test/cloudTest.lua
@@ -3,7 +3,7 @@ local function ClickBlock(event)
     local libname = "test" -- 组名
     local value = 10 -- 值
     local playerId = 123456 -- 私有变量组所属玩家id，0代表全局变量组
-    local data = {num = "10"}
+    local data = {featherNum = 0, skins = {1, 2, 3}, props = {23, 33, 0}}
     local ret = CloudSever:setDataListBykey("test", playerId, data)
     if ret == ErrorCode.OK then
         print('设置排行榜值成功 k = 123456 ,v = {}')
@@ -12,31 +12,67 @@ local function ClickBlock(event)
     end
 
 end
-local callback = function(ret, k, v)
-    print("ret=", ret)
-    print("k=", k)
-    print("v=", v)
+local getData = function(ret, k, v)
 
-    Chat:sendSystemMsg("ret=" .. ret)
-    Chat:sendSystemMsg("k=" .. k)
-    Chat:sendSystemMsg("v=" .. v)
     if ret == true then
-        print('返回数据成功 键= ' .. k .. ' 值=' .. v)
+        print("v.featherNum = ", v.featherNum)
+        print("v.skins = ", v.skins)
+        print("v.props = ", v.props)
     else
-        print('返回数据失败')
+        print('返回表数据失败')
+        initGamer()
+    end
+
+end
+local getUserData = function(libvarname, playerId)
+    local userData = {featherNum = 0, skins = {}, props = {}}
+    local function initGamer()
+        CloudSever:setDataListBykey("test", tostring(playerId), userData)
+    end
+    local ret = CloudSever:getDataListByKey(libvarname, playerId,
+                                            function(ret2, k, v, ix)
+
+        -- if ret ~= 0 or ret2 ~= true then -- 由于数据是云端的，所以要判断玩家是否初始化过，没有则初始化
+        if ret2 ~= true then -- 由于数据是云端的，所以要判断玩家是否初始化过，没有则初始化
+            print('返回表数据失败')
+            initGamer()
+        else
+            print("v.featherNum = ", v.featherNum)
+            print("v.skins = ", v.skins)
+            print("v.props = ", v.props)
+        end
+    end) -- 获取key1的分数
+
+    if ret == ErrorCode.OK then
+        print('请求test表数据成功')
+
+    else
+        print("请求test表数据失败")
     end
 end
 
-local function Game_StartGame(event)
+-- 玩家移动一格
+Player_MoveOneBlockSize = function(event)
     local libvarname = 'test'
     local playerId = 123456 -- 私有变量组所属玩家id，0代表全局变量组
-    local ret = CloudSever:getDataListByKey(libvarname, playerId, callback) -- 获取key1的分数
-    if ret == ErrorCode.OK then
-        print('请求排行榜数据成功')
-    else
-        print("请求排行榜数据失败")
-    end
+    getUserData(libvarname, playerId)
+
 end
+-- 任一玩家进入游戏
+Game_AnyPlayer_EnterGame = function(event)
+    Chat:sendSystemMsg("玩家进入游戏")
+    -- 初始化玩家信息
+    local playerId = event.eventobjid
+    local libvarname = 'test'
+    getUserData(libvarname, playerId)
+
+end
+
 ScriptSupportEvent:registerEvent([=[Player.ClickBlock]=], ClickBlock) -- 点击方块
 -- 游戏事件---
-ScriptSupportEvent:registerEvent([=[Game.Start]=], Game_StartGame)
+-- 任一玩家进入游戏	
+ScriptSupportEvent:registerEvent([=[Game.AnyPlayer.EnterGame]=],
+                                 Game_AnyPlayer_EnterGame)
+-- 玩家移动一格
+ScriptSupportEvent:registerEvent([=[Player.MoveOneBlockSize]=],
+                                 Player_MoveOneBlockSize)
